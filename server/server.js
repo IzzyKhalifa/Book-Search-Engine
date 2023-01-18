c08306a7dafa9c09cefb1f33034b38a67c77e61b
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2023-01-18 19:29:00 +1100

    Added queries and mutations

--- server/server.js
+++ server/server.js
@@ -7,13 +7,17 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+let apolloServer = null;
+async function startServer() {
+  apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+}
 
-server.applyMiddleware({ app });
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,5 +32,5 @@ app.get("*", (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  console.log(`GraphQL server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`);
 });
